944d2a449de5d149478d7258a4f07e8862ae91f7
kdh24 <java_24@naver.com>
2017-05-21 16:15:37 +0900

    zipcode 데이터 불러와서 xml 파일 생성

--- kopoProject/test2/src/CalcBonusProject/Calc_Bonus_by_stmt_9.java
+++ kopoProject/test2/src/CalcBonusProject/Calc_Bonus_by_stmt_9.java
@@ -8,7 +8,7 @@ import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.Statement;
 
-public class Calc_Bonus_by_stmt_8 {
+public class Calc_Bonus_by_stmt_9 {
 	public static void main(String[] args) {
 		try {
 			// 오라클 DB에 연결
@@ -34,7 +34,7 @@ public class Calc_Bonus_by_stmt_8 {
 			int count = 1;
 			boolean debugStatus = false;
 
-			sqlStr = "DECLARE"
+		/*	sqlStr = "DECLARE"
 					+ " CURSOR CUR_BONUS IS  SELECT EMPNO, JOB, DEPTNO, SAL FROM EMP_LARGE "
 					+ " WHERE JOB NOT LIKE 'PRESIDENT';"
 					+ "TYPE R_CUR_BONUS IS TABLE OF CUR_BONUS%ROWTYPE;"
@@ -74,7 +74,15 @@ public class Calc_Bonus_by_stmt_8 {
 					+ "SQL_CODE:= SUBSTR(SQLCODE,1,200);"
 					+ "INSERT INTO ERROR_LOG_TBL(ERROR_TIME, PROGRAM_NAME, ERROR_CODE, ERROR_MESSAGE, ETC)"
 					+ " VALUES(SYSDATE, '프로그램 에러',SQL_CODE, SQL_ERRM, '비고');"
-					+ "END;";
+					+ "END;";*/
+			sqlStr = "INSERT INTO BONUS_LARGE(YYYYMM,EMPNO,JOB,DEPTNO,SAL,BONUS)"
+					+ " SELECT TO_CHAR(SYSDATE, 'YYYYMM'), EMPNO, JOB, DEPTNO, SAL,"
+					+ " DECODE(DEPTNO, 40, ROUND(SAL * 0.2),"
+					+ " 30, ROUND(SAL * 0.05),"
+					+ " 20, ROUND(SAL * 0.1),"
+					+ " 10, ROUND(SAL * 0.3))"
+					+ " FROM EMP_LARGE"
+					+ " WHERE JOB NOT LIKE 'PRESIDENT'";
 			
 			// 작업 처리를 위한 Statement 객체  생성
 			cstmt = conn.prepareCall(sqlStr);
@@ -83,17 +91,18 @@ public class Calc_Bonus_by_stmt_8 {
 			startTime = System.currentTimeMillis();
 			
 			// BULK 단위 지정을 위한 변수 선언 및 값 할당
-			int limit_num = 10000;
+//			int limit_num = 10000;
 			// CallableStatement 에 저장된 sql위치에 값을 할당해준다
-			cstmt.setInt(1, limit_num);
-			cstmt.setString(2, "201705");
+//			cstmt.setInt(1, limit_num);
+//			cstmt.setString(2, "201705");
 			cstmt.execute();
 
 			// 시간 측정을 위한 endTime 변수
 			endTime = System.currentTimeMillis();
 
 			// 총 작업 시간 출력
-			System.out.println( limit_num+"개 단위 경과 시간 : " + (endTime - startTime) + "ms");
+//			System.out.println( limit_num+"개 단위 경과 시간 : " + (endTime - startTime) + "ms");
+			System.out.println((endTime - startTime) + "ms");
 
 			// 작업이 끝난 후에 객체를 전부 종료해 준다
 			cstmt.close();
---
+++ kopoProject/test2/src/xml/XmlProject.java
@@ -0,0 +1,77 @@
+package xml;
+
+import java.io.File;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.Statement;
+
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.transform.TransformerFactory;
+import javax.xml.transform.dom.DOMSource;
+import javax.xml.transform.stream.StreamResult;
+
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;
+import org.w3c.dom.Node;
+
+public class XmlProject {
+	public static void main(String[] args) {
+		try {
+			// 오라클 DB에 연결
+			Class.forName("oracle.jdbc.driver.OracleDriver");
+			Connection conn = DriverManager.getConnection("jdbc:oracle:thin:@192.168.136.128:1521:orasparta", "SCOTT", "tiger");
+			
+			// 자동 commit 기능 해제
+			conn.setAutoCommit(false);
+			// 작업 처리를 위한 Statement 객체 생성
+			Statement stmt = conn.createStatement();
+
+			// 반복 횟수 처리를 위한 변수 생성
+			int count = 1;
+
+			// SELECT 문을 보내 값을 ResultSet 변수에 받아온다
+			ResultSet rs = stmt.executeQuery("SELECT SIDO, COUNT(DONG) FROM ZIPCODE"
+											+ " GROUP BY SIDO");
+
+			// Document 및 XML 트리 생성
+			Document document = DocumentBuilderFactory.newInstance().newDocumentBuilder().newDocument();
+			Node root = document.createElement("zipcode");
+			document.appendChild(root);
+			
+			while (rs.next()) {
+				// 처리 상황 확인하는 출력문
+				System.out.println(rs.getString(1) + " " + rs.getInt(2) + "\n");
+
+				// zipcode 테이블에서 받아온 값을 xml 형식에 맞게 넣어준다
+				Element address = document.createElement("address");
+				address.setAttribute("id", String.valueOf(count));
+				address.setAttribute("sido", rs.getNString(1));
+				root.appendChild(address);
+				{
+					Element sido = document.createElement("sido");
+					sido.appendChild(document.createTextNode(rs.getString(1)));
+					address.appendChild(sido);
+				}
+				{
+					Element dongCount = document.createElement("dongCount");
+					dongCount.appendChild(document.createTextNode(String.valueOf(rs.getInt(2))));
+					address.appendChild(dongCount);
+				}
+
+				count++;
+			}
+
+			// Document 저장
+			DOMSource xmlDOM = new DOMSource(document);
+			StreamResult xmlFile = new StreamResult(new File("c:\\xml\\saved3.xml"));
+			TransformerFactory.newInstance().newTransformer().transform(xmlDOM, xmlFile);
+			
+			// 작업이 끝난 후에 객체를 전부 종료해 준다
+			rs.close();
+			conn.close();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+}
