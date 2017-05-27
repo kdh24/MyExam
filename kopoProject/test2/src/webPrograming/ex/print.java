0d6693e58b8437bf5c88f8558c2e57d2dec400b5
kdh24 <kdh24@kdh24-PC>
2017-05-28 00:01:25 +0900

    영수증 주석 달기

--- kopoProject/test2/src/webPrograming/ex/print.java
+++ kopoProject/test2/src/webPrograming/ex/print.java
@@ -6,6 +6,7 @@ import java.util.Calendar;
 
 public class print {
 	public static void main(String[] args) {
+		// 숫자를 String 값으려 출력할때 1000(천) 단위마다 콤마(,)를 표시하도록 정해준다.
 		DecimalFormat k07_df = new DecimalFormat("###,###,###,###,###");
 		
 		// 가격 변수
@@ -15,16 +16,20 @@ public class print {
 		// 물건 세금
 		double k07_tax = k07_iPrice - k07_beforMoney;
 		
+		// 카드계산 변수 0이면 현금으로 계산하는 것으로 처리
 		int k07_card = k07_iPrice;
+		// 현금계산 변수
 		int k07_cash = 0;
+		// 카드 변수가 0으로 초기화가 되면 현금계산이므로 변경처리
 		if(k07_card == 0)
 			k07_cash = k07_iPrice;
-		else
-			k07_cash = 0;
-		
+
+		// 날짜 계산을 위해 칼렌다 변수를 선언 및 생성한다
 		Calendar k07_calt = Calendar.getInstance();
+		// 현재 날짜 및 시간을 어떤 포맷으로 사용할지 포맷을 정해준다
 		SimpleDateFormat k07_sdt = new SimpleDateFormat("YYYY-MM-dd HH:mm:ss");
 
+		// 영수증 출력 부분 길이를 정해 정해진 텍스트를 그대로 출력해준다
 		System.out.printf("%30.30s\n", "영   수   증");
 		System.out.printf("-----------------------------------------------------\n");
 		System.out.printf("%-100.100s\n", "종로상회(분당서현점)  129-17-77924");
@@ -33,7 +38,8 @@ public class print {
 		System.out.printf("%-100.100s\n", "테이블명 : 12");
 		System.out.printf("%-100.100s\n", "주문번호 : 20160425 01 00041");
 		System.out.printf("-----------------------------------------------------\n");
-//		System.out.printf("%-86.86s%d\n", "주문합계", k07_iPrice);
+		// 한줄에 출력부분을 2개로 나누어 30.30s 부분은 항목을 19.19s 부분은 금액을 표기
+		// df.format을 사용해서 금액을 String 방식으로 표기하고 1000(천) 단위마다 콤마(,) 표기
 		System.out.printf("%-30.30s", "주문합계");
 		System.out.printf("%19.19s\n",  k07_df.format(k07_iPrice));
 		System.out.printf("%-30.30s", "할인금액");
@@ -41,6 +47,8 @@ public class print {
 		System.out.printf("%-30.30s", "받을금액");
 		System.out.printf("%19.19s\n",  k07_df.format(k07_iPrice));
 		System.out.printf("-----------------------------------------------------\n");
+		// 위에서 계산한 세전금액, 부가세, 현금 or 카드 계산을 변수로해서 각각 출력해준다
+		// 마찬가지로 df.format을 사용해 천단위 콤마 표기
 		System.out.printf("%-18.18s %6.6s", "현   금", k07_df.format(k07_cash));
 		System.out.printf("%10.10s%14.14s\n", "과   세", k07_df.format(k07_beforMoney));
 		System.out.printf("%-18.18s %6.6s", "카   드", k07_df.format(k07_card));
@@ -49,6 +57,8 @@ public class print {
 		System.out.printf("%10.10s%14.14s\n", "면   세", k07_df.format(0));
 		System.out.printf("%35.35s%14.14s\n", "영수금액", k07_df.format(k07_iPrice));
 		System.out.printf("-----------------------------------------------------\n");
+		// 위에서 현금과 카드변수를 따로 만들었기 때문에 카드 승인 부분은
+		// 카드변수가 0이 아닐때(카드결제일때)만 출력하게 해준다
 		if(k07_card != 0){
 			System.out.printf("%28.28s\n", "[우리카드 신용 승인]");
 			System.out.printf("%-14.14s%20.20s\n", "승  인  일  시  : ", k07_sdt.format(k07_calt.getTime()));
--- kopoProject/test2/src/webPrograming/ex/print3_1.java
+++ kopoProject/test2/src/webPrograming/ex/print3_1.java
@@ -5,61 +5,83 @@ import java.text.SimpleDateFormat;
 import java.util.Calendar;
 
 public class print3_1 {
+	// 한글 숫자를 계산하는 한글 카운터 클래스
+	// 매개변수로 String 값을 넘겨받아 처리해준뒤 한글 갯수가 몇개인지 int형 값으로 리턴해준다
 	public static int k07_HanCount(String k07_Han) {
+		// 매개변수 Han 변수에 한글이 몇개나 있는지 확인해서 int 형 갯수를 저장하기 위한 변수
+		int k07_count = 0;
 
-	int k07_count=0;
-	
-	for(int i=0; i<k07_Han.length(); i++){
-		if(k07_Han.substring(i, i+1).getBytes().length >1){
-			k07_count += 1;
+		// String 변수 Han의 길이만큼 반복해서 연산하는 for 반복문
+		for (int i = 0; i < k07_Han.length(); i++) {
+			// 정해준 위치의 값을 가져오는 substring 변수를 이용해서
+			// i값부터 i+1의 값을 계속 가져와 처음부터 끝까지 한글자씩 가져오고
+			// 그 바이트의 값이 1을 넘으면 한글(2바이트)이므로 if문을 실행시킨다
+			if (k07_Han.substring(i, i + 1).getBytes().length > 1) {
+				// 한글일때 실행하므로 갯수를 증가시켜준다
+				k07_count += 1;
+			}
 		}
+		// 한글 갯수를 리턴값으로 넘겨준다
+		return k07_count;
 	}
-	
-	return k07_count;
-}
+
 	public static void main(String[] args) {
+		// 숫자를 String 값으려 출력할때 1000(천) 단위마다 콤마(,)를 표시하도록 정해준다.
 		DecimalFormat k07_df = new DecimalFormat("###,###,###,###,###");
-		
+		// 날짜 계산을 위해 칼렌다 변수를 선언 및 생성한다
 		Calendar k07_cal = Calendar.getInstance();
+		// 현재 날짜 및 시간을 어떤 포맷으로 사용할지 포맷을 정해준다		
 		SimpleDateFormat sdf = new SimpleDateFormat("YYYY/MM/dd HH:mm");
 		
+		// 구매할 품목명을 String 배열에 순차적으로 30개를 저장시켜 놓았다
 		String [] k07_itemName = {"초코파이", "바나나우유", "건포도", "오렌지주스", "초코에몽",
 							"칠성사이다", "우유속모카", "맥스봉", "빼빼로", "볼펜",
 							"서울우유1000L", "짜파게티", "신라면", "햇반", "좋은데이",
 							"요플레", "어묵400g", "오뚜기케찹", "오뚜기진라면", "두부200g",
 							"콘푸로스트", "맥심카누", "참이슬", "오비맥주", "칭따오",
 							"안성탕면", "제주삼다수", "농심백산수", "호떡믹스", "파프리카"}; // 항목명 30개이상
+		// 구매할 품목의 가격에 해당하는 금액을 int형 배열에 30개 저장
 		int [] k07_price = {4000, 1000, 3300, 2500, 800,
 						1700, 1500, 1000, 1200, 700,
 						2900, 1400, 1500, 2000, 1700,
 						3500, 1300, 2800, 1400, 1500,
 						4000, 2200, 1600, 2500, 3000,
 						1500, 1000, 1100, 3000, 2600};
+		// 구매할 상품을 몇개나 살지 int형 배열에 저장시켰다
 		int [] k07_num={2,4,1,3,5,
 					3,6,2,1,2,
 					3,4,2,1,1,
 					2,3,4,1,1,
 					3,5,6,2,3,
 					2,1,1,3,4};
+		// 구매할 품목의 상품이 면세상품인지 과세상품인지 true와 false로 구분해서 
+		// boolean형 배열에 순차적으로 저장했다
 		boolean [] k07_taxfree={true, true, false, true, true,
 				true, true, false, true, true,
 				true, true, true, true, false,
 				true, true, true, true, true,
 				false, true, false, true, true,
 				true, true, false, true, false};
-		
+		// 상품의 세전 가격을 저장할 변수
 		int k07_beforeTax =0;
+		// 상품별 세전가격의 누적값을 저장할 변수
 		int k07_beforeTaxSum = 0;
+		// 과세 상품의 세금값을 저장할 변수
 		int k07_tax = 0;
+		// 면세 상품의 누적가격을 저장할 변수
 		int k07_freeSum=0;
+		// 과세상품의 세금값을 누적시킬 변수
 		int k07_taxSum=0;
+		// 물건의 총 금액을 저장할 변수
 		int k07_sum=0;
 
+		// 출력을 원하는 크기만큼 해당 텍스트를 출력해준다
 		System.out.printf("%-30.30s\n", "영수증 미지참시 교환/환불 불가(30일내)");
 		System.out.printf("%-30.30s\n", "교환/환불 구매점에서 가능(결제카드지참)");
 		System.out.printf("%-30.30s\n", "체크카드/신용카드 청구취소 반영은");
 		System.out.printf("%-30.30s\n\n", "최대 3''5일 소요 (주말,공휴일제외)");
 
+		// 현재 날짜 및 시간을 
 		System.out.printf("%-6.6s%-20.20s %-13.13s\n", "[구  매]", sdf.format(k07_cal.getTime()), "POS:0009-2418");
 		System.out.printf("------------------------------------------------\n");
 		System.out.printf("%10.10s %10.10s %8.8s %8.8s\n", "상  품  명", "단  가", "수  량", "금  액");
